995c57da38e6fdec6aed0485cd05e49c8085a8d8
Ironside <iIronside@yandex.ua>
2017-02-23 23:31:28 +0300

    11 needs work

--- Lab1/All Task/task11.py
+++ Lab1/All Task/task11.py
@@ -1,1 +1,11 @@
+# Напишите генератор frange как аналог range() с дробным шагом.
+
+
+def frange(stop, start=0.0, step=0.1):
+    while start < stop:
+        yield round(start + step, 1)
+        start += step
+
+for x in frange(5, 1, 0.1):
+    print(x)
 help(range)
